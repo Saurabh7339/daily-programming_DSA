f78f4f1d47c30c3c3cf4b1004befa8fe4ab63c29
saurabh <saurabhkr7339@gmail.com>
2024-11-13 20:48:25 +0530

    added code for leetcode  best time to buy and sell stock

---
+++ DSA_using_Python/best_time_to_buy_and_sell_II.py
@@ -0,0 +1,11 @@
+def maxProfit(prices) -> int:
+    max_profit = 0
+    for i in range(1,len(prices)):
+        if prices[i]>prices[i-1]:
+            print(prices[i])
+            max_profit = max_profit + ( prices[i] - prices[i-1] )
+    return max_profit
+
+
+price_list = [7,1,5,3,6,4]
+print(maxProfit(price_list))
---
+++ DSA_using_Python/main.py
@@ -0,0 +1,5 @@
+nums = [1,1,2]
+my_set = set()
+for i in nums:
+    my_set.add(i)
+print(list(my_set))
